494ecf77169d1fc5b971a961076cdb3d9d290cfd
fagao88 <67219812+fagao88@users.noreply.github.com>
2025-09-21 04:04:55 +0800

    Update main.tsx
    Add SpeedInsights component

--- 週末股票交易計畫表/src/main.tsx
+++ 週末股票交易計畫表/src/main.tsx
@@ -1,9 +1,14 @@
 
-  import { createRoot } from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
-  import { SpeedInsights } from '@vercel/speed-insights/react';
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
+createRoot(document.getElementById("root")!).render(
+  <>
+    <App />
+    <SpeedInsights />
+  </>
+);
 
-  createRoot(document.getElementById("root")!).render(<App />);
   
